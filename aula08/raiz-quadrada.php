49c5402daa941cbe1c85ad74bb1aacc2a9bf9958
alef <alef.ss@proton.me>
2025-09-02 09:10:13 -0300

    integração HTML5 + PHP

---
+++ aula08/raiz-quadrada.php
@@ -0,0 +1,14 @@
+<?php
+$numero = $_GET["numero"];
+
+$raiz_quadrada = sqrt($numero);
+echo $raiz_quadrada;
+
+/* 
+  A família é a coisa mais preciosa
+  para o ser humano, o desejo de
+  protegê-la faz de uma alma dócil
+  um combatente de guerra.
+
+  Inspirado em Edward Newgate (Barba Branca)
+*/
